6c01b2b93356340de07ce64cbdac68c226e3e9ac
Khoi Dinh <khoiddinh@gmail.com>
2025-11-20 00:27:25 -0500

    fixed models field

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
-import enum
 # Create your models here.
 
 class QuestionType(models.TextChoices):
