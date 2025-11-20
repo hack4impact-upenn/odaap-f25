b4db5fb98e106c636a5f5ff04a9a2d0a3c5e73bf
Grace <graceli3245@gmail.com>
2025-11-20 00:26:21 -0500

    cursor cooked w routes

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -15,17 +15,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 # views
-from core.views import CourseViewSet
+from core.views import CourseViewSet, ModuleViewSet
 
+# Create router and register viewsets
+router = DefaultRouter()
+router.register(r'courses', CourseViewSet, basename='course')
+router.register(r'modules', ModuleViewSet, basename='module')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    # courses
-    path('api/courses/', CourseViewSet.as_view(), name='course_list'),
+    # Include router URLs (provides all ViewSet endpoints)
+    path('api/', include(router.urls)),
 ]
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,10 +1,10 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from .models import Course, User, CourseToStudents, CourseToTeachers, Module
-from .serializers import CourseSerializer
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
+from django.shortcuts import get_object_or_404
+from .models import Course, CourseToStudents, CourseToTeachers, Module
+from .serializers import CourseSerializer, ModuleSerializer
 
 # Course ViewSet using ModelViewSet
 class CourseViewSet(viewsets.ModelViewSet):
@@ -13,10 +13,75 @@ class CourseViewSet(viewsets.ModelViewSet):
 
     # gets the courses the user is enrolled in
     def get_queryset(self):
-        if self.request.user.is_student:
-            return CourseToStudents.objects.filter(students=self.request.user)
+        user = self.request.user
+        
+        # Check if user is a student (field is isStudent, not is_student)
+        if user.isStudent:
+            # Get courses where user is a student
+            # Django creates reverse relationship: CourseToStudents has ForeignKey to Course,
+            # so we can query Course.objects.filter(coursetostudents__user=user)
+            # This is equivalent to: get all Courses that have a CourseToStudents entry with this user
+            return Course.objects.filter(coursetostudents__user=user).distinct()
+            
+            # Alternative approach (less efficient, but shows the relationship):
+            # course_students = CourseToStudents.objects.filter(user=user)
+            # course_ids = [cs.course_id for cs in course_students]
+            # return Course.objects.filter(id__in=course_ids)
         else:  # teacher
-            return CourseToTeachers.objects.filter(teachers=self.request.user)
+            # Get courses where user is a teacher
+            # Same concept: query Course backwards through CourseToTeachers relationship
+            return Course.objects.filter(coursetoteachers__user=user).distinct()
+    
+    @action(detail=True, methods=['get'], url_path='modules')
+    def get_course_modules(self, request, pk=None):
+        """
+        Get all modules for a specific course.
+        Usage: GET /api/courses/{course_id}/modules/
+        """
+        course = self.get_object()  # Gets the course by pk
+        modules = Module.objects.filter(course=course)
+        serializer = ModuleSerializer(modules, many=True)
+        return Response(serializer.data)
+
+class ModuleViewSet(viewsets.ModelViewSet):
+    serializer_class = ModuleSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """
+        Get modules filtered by course_id from query parameter.
+        Usage: GET /api/modules/?course_id=1
+        """
+        queryset = Module.objects.all()
+        
+        # Get course_id from query parameters
+        course_id = self.request.query_params.get('course_id', None)
+        
+        if course_id:
+            # Verify user has access to this course
+            user = self.request.user
+            if user.isStudent:
+                user_courses = Course.objects.filter(coursetostudents__user=user)
+            else:
+                user_courses = Course.objects.filter(coursetoteachers__user=user)
+            
+            # Check if the requested course_id is in user's courses
+            if user_courses.filter(id=course_id).exists():
+                queryset = queryset.filter(course_id=course_id)
+            else:
+                # User doesn't have access to this course, return empty queryset
+                return Module.objects.none()
+        else:
+            # If no course_id provided, return all modules from user's courses
+            user = self.request.user
+            if user.isStudent:
+                user_courses = Course.objects.filter(coursetostudents__user=user)
+            else:
+                user_courses = Course.objects.filter(coursetoteachers__user=user)
+            
+            queryset = queryset.filter(course__in=user_courses)
+        
+        return queryset
         
 
 
